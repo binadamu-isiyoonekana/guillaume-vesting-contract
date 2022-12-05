42ba5901d156a80262fe34c95b9a456e425cb8d8
GuiBL4 <gl.bonnaud@outlook.com>
2022-12-05 13:06:54 +0100

    First commit

--- SoFairTokenSupply.sol
+++ SoFairTokenSupply.sol
@@ -7,7 +7,8 @@ contract SoFairSupply is ERC1155 {
     uint256 public constant SoFairToken = 0; //Id of the very first token released
     uint256 public constant tokenAmountMinted = 70 * 10**6;
 
-    constructor() ERC1155("{id}") {
-        _mint(address(this), SoFairToken, tokenAmountMinted, ""); // Token is available at this contract address
+    // No URI specified in the constructor. Must be added here if one is provided
+    constructor() ERC1155("") {
+        _mint(msg.sender, SoFairToken, tokenAmountMinted, ""); //Tokens are minted on the sender's address
     }
 }
